868849e39c7af2a78d9d4d4c498795b2425c074a
rayliaw <ray.liaw@gmail.com>
2015-09-10 10:04:49 -0700

    bookazon controllers

--- 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
+++ 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
@@ -6,25 +6,48 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = get_book
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @book = get_book
   end
 
   def update
+    @book = get_book
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @book = get_book
+    @book.destroy
+    redirect_to books_path
   end
 
   private
 
   def book_params
+    params.require(:book).permit(:title, :year_published, :author, :available, :genre, :image_url)
+  end
+
+  def get_book
+    Book.find(params[:id])
   end
 end
